e81cd5fb4b0d28cedb5cf67d92d4c92c9b262a98
Ehsan Javaid <ehsanjavaid321@gmail.com>
2025-03-10 09:20:03 +0500

    feature: Vehicle type

--- rentals/rentals/doctype/driver/test_driver.py
+++ rentals/rentals/doctype/driver/test_driver.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2025, Ahsan and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestDriver(FrappeTestCase):
-	pass
+    def test_full_name_correctly_set(self):
+        test_driver = frappe.new_doc("Driver")
+        test_driver.first_name = "John"
+        test_driver.last_name = "Doe"
+        test_driver.license_number = "123456"
+        test_driver.save()
+        self.assertEqual(test_driver.full_name, "John Doe")
+        self.assertEqual(test_driver.license_number, "123456")
+        
+    # def test_full_name_correctly_set_when_last_name_not_set(self):
+    #     test_driver = frappe.new_doc("Driver")
+    #     test_driver.first_name = "John"
+    #     test_driver.license_number = "123456"
+    #     test_driver.save()
+    #     self.assertEqual(test_driver.full_name, "John")
+    #     self.assertEqual(test_driver.license_number, "123456")
+
+	
+		
+	
